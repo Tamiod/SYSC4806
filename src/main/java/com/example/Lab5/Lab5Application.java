504d4153a4f0c72fc54b5b005c3e0711125111dd
tamiloreodunlami <elozotami@gmail.com>
2021-02-28 11:27:05 -0500

    Added Tests

--- src/main/java/com/example/Lab5/Lab5Application.java
+++ src/main/java/com/example/Lab5/Lab5Application.java
@@ -1,13 +1,13 @@
-package com.example.Lab5;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Lab5Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Lab5Application.class, args);
-	}
-
-}
+//package com.example.Lab5;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Lab5Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Lab5Application.class, args);
+//	}
+//
+//}
---
+++ src/main/java/com/example/Lab5/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab5;
+import org.testng.annotations.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
